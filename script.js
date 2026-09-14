851648becef59e5c51e4002d349c49dae61e8181
Siddhi Bhagwat <siddhi.bhagwat09@gmail.com>
2026-09-14 17:56:34 +0000

    Add gui to the game Add results and score feature

--- script.js
+++ script.js
@@ -9,66 +9,97 @@ function getComputerChoice()
 
 let humanScore = 0
 let computerScore = 0
+
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector(".scissors");
+const result = document.querySelector(".result");
+const score = document.querySelector(".score");
+
 //Checks winner of the round and informs the player
-function playRound(c, h)
+function playRound(h)
 {
+    const c = getComputerChoice();
     h = h.toLowerCase();
     if(c == h)
     {
         console.log("Tie");
+        result.textContent = "Tie";
     }
 
     else if(c === "rock" && h === "paper" || c === "paper" && h === "scissors" || c === "scissors" && h === "rock")
     {
         console.log(`You win! ${h} beats ${c}`);
+        result.textContent = `You win! ${h} beats ${c}`;
         humanScore++;
     }
 
     else{
         console.log(`You lose! ${c} beats ${h}`);
+        result.textContent = `You lose! ${c} beats ${h}`
         computerScore++;
     }
+    score.textContent = `You: ${humanScore} | Computer: ${computerScore}`;
+
+    if(humanScore === 5){
+        result.textContent = "Congratulations! You won the game!";
+        endGame();
+    }
+
+    else if(computerScore === 5){
+        result.textContent = "You lost the game this time :(";
+        endGame();
+    }
+}
+
+//Disable buttons when game is over
+function endGame()
+{
+    rock.disabled = true;
+    paper.disabled = true;
+    scissors.disabled = true;
 }
 
 //Logs final scores and winner of the game
-function getWinner()
-{
-    // Log final scores
-    console.log("Your final score is: " + humanScore);
-    console.log("Computer's final score is: " + computerScore);
+// function getWinner()
+// {
+//     // Log final scores
+//     console.log("Your final score is: " + humanScore);
+//     console.log("Computer's final score is: " + computerScore);
 
-    // Log winner
+//     // Log winner
 
-    // User wins
-    if (humanScore > computerScore)
-    {
-        console.log("Congratulations! You won the game");
-    }
+//     // User wins
+//     if (humanScore > computerScore)
+//     {
+//         console.log("Congratulations! You won the game");
+//     }
 
-    // Computer wins
-    else if (computerScore > humanScore)
-    {
-        console.log("You lost the game this time :(");
-    }
+//     // Computer wins
+//     else if (computerScore > humanScore)
+//     {
+//         console.log("You lost the game this time :(");
+//     }
 
-    // Tie
-    else
-    {
-        console.log("No winner! The game is a tie");
-    }
-}
+//     // Tie
+//     else
+//     {
+//         console.log("No winner! The game is a tie");
+//     }
+// }
 
-//Starts the game and 5 rounds played
-function playGame()
-{
-    for(let i = 0; i < 5; i++)
-    {
-        const computerValue = getComputerChoice();
-        const humanValue = prompt("Enter your choice");
+// playGame();
 
-        playRound(computerValue, humanValue);
-    }
-    getWinner();
-}
 
-playGame();
+
+rock.addEventListener("click", function(e){
+    playRound("rock");    
+});
+
+paper.addEventListener("click", function(e){
+    playRound("paper");    
+});
+
+scissors.addEventListener("click", function(e){
+    playRound("scissors");    
+});
